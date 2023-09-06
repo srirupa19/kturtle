eb495335408995671db92d0f90a07e3222e66c77
Srirupa Datta <srirupa.data@outlook.com>
2023-09-06 23:20:58 +0530

    Clean code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,7 +15,7 @@ export class AppComponent implements OnInit{
 
   editorConfig = {
     toolbar: [],
-    lineNumbers: true, // Enable line numbers
+    lineNumbers: true, 
   };
 
   constructor(public turtleService: TurtleService) {}
@@ -25,30 +25,23 @@ export class AppComponent implements OnInit{
   }
 
   async executeCommand(): Promise<void> {
-    // this.turtleService.executeCommand(this.commandInput);
-    // this.commandInput = '';
 
     try {
-      // Attempt to execute the command
       await this.turtleService.executeCommand(this.commandInput);
 
-      // If successful, set executionResult to a success message
       this.executionResult = 'Code executed without any errors.';
     } catch (error) {
-      // If there's an error, set executionResult to the error message
-      // this.executionResult = error.message;
       this.executionResult = String(error);
     }
   }
 
   onSpeedChange(): void {
-    // Update the animation speed in the TurtleService
     this.turtleService.animationSpeed = this.animationSpeed;
   }
 
   clearTextArea(): void {
     this.turtleService.resetTurtlePosition();
-    this.commandInput = ''; // Clear the text area
+    this.commandInput = ''; 
     this.executionResult = '';
   }
 
